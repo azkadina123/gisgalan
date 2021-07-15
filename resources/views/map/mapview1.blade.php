6bc4e84da40fc68a89b583315c1a0d9342dfb872
azkadina123 <dinaazka870@gmail.com>
2021-07-15 08:26:42 -0700

    perbulan

--- resources/views/map/mapview1.blade.php
+++ resources/views/map/mapview1.blade.php
@@ -98,8 +98,28 @@
                             <select class="form-control select2bs4" name="tanggal_input" style="width: 100%;">
                                 <option selected="selected" disabled>-- Pilih Semester --</option>
                                 <option value="semua">Semua</option>
-                                <option value="balita">Januari-Juni</option>
-                                <option value="remaja">Juli-Desember</option>
+                                <option value="1">Januari</option>
+                                <option value="2">Februari</option>
+                                <option value="3">Maret</option>
+                                <option value="4">April</option>
+                                <option value="5">Mei</option>
+                                <option value="6">Juni</option>
+                                <option value="7">Juli</option>
+                                <option value="8">Agustus</option>
+                                <option value="9">September</option>
+                                <option value="10">Oktober</option>
+                                <option value="11">November</option>
+                                <option value="12">Desember</option>
+
+
+
+
+
+
+
+
+
+
                             </select>
                         </div>
                         <button class="btn btn-primary waves-effect" type="button" id="filter-data">CARI</button>
